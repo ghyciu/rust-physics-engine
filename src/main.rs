065bd56827a2b996e7eed5d3c3e9acf1dd5ce433
Gio Ciudadano <gcciudadano@gmail.com>
2026-03-04 15:35:33 +0800

    Add generic for mul and div point2d operation

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,11 @@ use physics::point_2d::Point2D;
 #[macroquad::main("Physics Engine")]
 async fn main() {
   let point_a: Point2D = Point2D::new(1, 2);
-  print!("{}", point_a);
+  let point_b: Point2D = Point2D::new(2, 3);
+  print!("{}", point_a + point_b);
+  print!("{}", point_a - point_b);
+  print!("{}", point_a * 2);
+  print!("{}", point_a / 2);
 
   let mut graphics_manager: GraphicsManager = GraphicsManager::new();
   graphics_manager.add_shape(Square::new());
--- src/physics/point_2d.rs
+++ src/physics/point_2d.rs
@@ -1,5 +1,8 @@
 use std::fmt;
 use std::ops::Add;
+use std::ops::Sub;
+use std::ops::Mul;
+use std::ops::Div;
 
 pub struct Point2D {
   x: f32,
@@ -23,8 +26,15 @@ impl Point2DValue for i32 {
 }
 
 impl Point2D {
-  pub fn new<T: Point2DValue>(x: T, y: T) -> Point2D {
+  pub fn new<T: Point2DValue, U: Point2DValue>(x: T, y: U) -> Point2D {
     Point2D { x: x.to_f32(), y: y.to_f32() }
+  }
+}
+
+impl Copy for Point2D {}
+impl Clone for Point2D {
+  fn clone(&self) -> Point2D {
+    *self
   }
 }
 
@@ -38,5 +48,28 @@ impl Add<Point2D> for Point2D {
   type Output = Point2D;
   fn add(self, other: Point2D) -> Point2D {
     Point2D::new(self.x + other.x, self.y + other.y)
+  }
+}
+
+impl Sub<Point2D> for Point2D {
+  type Output = Point2D;
+  fn sub(self, other: Point2D) -> Point2D {
+    Point2D::new(self.x - other.x, self.y - other.y)
+  }
+}
+
+impl<T: Point2DValue> Mul<T> for Point2D {
+  type Output = Point2D;
+  fn mul(self, other: T) -> Point2D {
+    let other_f32 = other.to_f32();
+    Point2D::new(self.x * other_f32, self.y * other_f32)
+  }
+}
+
+impl<T: Point2DValue> Div<T> for Point2D {
+  type Output = Point2D;
+  fn div(self, other: T) -> Point2D {
+    let other_f32 = other.to_f32();
+    Point2D::new(self.x / other_f32, self.y / other_f32)
   }
 }
